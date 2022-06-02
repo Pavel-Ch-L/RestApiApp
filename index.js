47d73dbeab5ba939d5be6c785c07398b054cd194
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-06-02 14:30:55 +0600

    88 Routes settings

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path')
+const toDoRoutes = require('./routes/toDo')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use( express.static(path.join(__dirname, 'public')) )
+
+app.use('/api/todo', toDoRoutes)
 
 app.use( (req, res, next) => {
   res.sendFile('/index.html')
